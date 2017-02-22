88a45c89d22de916ed681caba4f3ee7a712bbc72
cxg8590 <cxg8590@rit.edu>
2017-02-22 13:21:05 -0500

    adding to github

--- src/server.js
+++ src/server.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const index = fs.readFileSync('client/index.html');
 
 const onRequest = (request, response) => {
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.write(index);
-	response.end();
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
 };
 
 const app = http.createServer(onRequest).listen(port);
@@ -19,32 +19,32 @@ console.log(`Listening on 127.0.0.1: ${port}`);
 const io = socketio(app);
 
 const onJoined = (sock) => {
-	const socket = sock;
-	
-	socket.on('join', (data) => {
-		socket.join(data);
-	});
+  const socket = sock;
+
+  socket.on('join', (data) => {
+    socket.join(data);
+  });
 };
 
 const onDrw = (sock) => {
-	const socket = sock;
-	
-	socket.on('draw', (data) => {
-		io.sockets.in('room1').emit('update', data);
-	});
+  const socket = sock;
+
+  socket.on('draw', (data) => {
+    io.sockets.in('room1').emit('update', data);
+  });
 };
 
 const onDisconnect = (sock) => {
-	const socket = sock;
-	socket.on('disconnect', () => {
-		socket.leave('room1');
-	});
+  const socket = sock;
+  socket.on('disconnect', () => {
+    socket.leave('room1');
+  });
 };
 
 io.sockets.on('connection', (socket) => {
-	console.log('started');
-	
-	onJoined(socket);
-	onDrw(socket);
-	onDisconnect(socket);
+  console.log('started');
+
+  onJoined(socket);
+  onDrw(socket);
+  onDisconnect(socket);
 });
